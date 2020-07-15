ad0a4a30115b111d6f17d95b1dae8e34c6853765
Kyrune <jn-01@centurytel.net>
2020-07-15 11:21:26 -0500

    Add JSX to map over pokemon list

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -8,6 +8,8 @@ export function PokemonsContainer() {
     });
 
     return (
-        
-    )
+        <div className="container">
+            {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+        </div>
+    );
 }
